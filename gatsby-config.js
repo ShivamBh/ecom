ae914a75b7326bd318c3fe55f85c50c2232ef11e
Shivam Bhattacharjee <shivam.bhattacharjee94@gmail.com>
2021-04-08 20:02:06 +0530

    carousel fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,7 @@ module.exports = {
     author: `@alexanderhorl`,
   },
   plugins: [
+    `gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-prismic`,
--- src/components/ProductCarousel/DetailPageCarousel.js
+++ src/components/ProductCarousel/DetailPageCarousel.js
@@ -1,27 +1,42 @@
 import React, { useState } from 'react'
 import Img from 'gatsby-image'
 import Slider from 'react-slick'
-// import {
-//   CarouselProvider,
-//   Slider,
-//   Slide,
-//   ButtonBack,
-//   ButtonNext,
-// } from 'pure-react-carousel'
+import SwiperCore, { Thumbs } from 'swiper'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/swiper.scss'
+import 'swiper/components/thumbs/thumbs.scss'
 import { WindowContainer, WindowItem, CarouselContainer } from './styles'
 import 'pure-react-carousel/dist/react-carousel.es.css'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 
+SwiperCore.use([Thumbs])
+
 const CarouselWindows = ({ images }) => {
   return (
     <>
-      <WindowContainer>
-        {images.map((image, index) => (
+      <WindowContainer className="swiper-thumbs">
+        <Swiper
+          slidesPerView={4}
+          autoHeight={true}
+          loop={true}
+          width={400}
+          spaceBetween={24}
+          centeredSlides={true}
+        >
+          {images.map((image, index) => (
+            <SwiperSlide key={image.id}>
+              <WindowItem>
+                <Img fluid={image.localFile.childImageSharp.fluid} />
+              </WindowItem>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+        {/* {images.map((image, index) => (
           <WindowItem key={image.id}>
             <Img fluid={image.localFile.childImageSharp.fluid} />
           </WindowItem>
-        ))}
+        ))} */}
       </WindowContainer>
     </>
   )
@@ -29,7 +44,9 @@ const CarouselWindows = ({ images }) => {
 
 const CarouselSlider = ({ images }) => {
   const settings = {
-    dots: false,
+    dots: true,
+    arrows: true,
+    swipe: true,
     infinite: true,
     autoplay: true,
     adaptiveHeight: true,
@@ -38,40 +55,47 @@ const CarouselSlider = ({ images }) => {
   }
   return (
     <>
-      {/* <Slider {...settings} className="overflow-hidden">
-        {images.map((image, index) => (
-          <Img fluid={image.localFile.childImageSharp.fluid} />
-        ))}
-      </Slider> */}
-
-      {/* <CarouselProvider
-        naturalSlideHeight={584}
-        naturalSlideWidth={467}
-        totalSlides={images.length}
-      >
-        
-        <ButtonBack>Back</ButtonBack>
-        <ButtonNext>Next</ButtonNext>
-      </CarouselProvider> */}
-      <Slider
+      {/* <Slider
         {...settings}
         className="overflow-hidden"
         style={{
           maxWidth: '480px',
-
           marginBottom: '1.33vw',
         }}
       >
         {images.map((image, index) => (
           <Img fluid={image.localFile.childImageSharp.fluid} key={image.id} />
         ))}
-      </Slider>
+      </Slider> */}
+      <Swiper
+        slidesPerView={1}
+        autoHeight={true}
+        loop={true}
+        width={400}
+        spaceBetween={40}
+        centeredSlides={true}
+        style={{
+          width: `480px`,
+        }}
+        thumbs={{
+          swiper: 'swiper-thumbs',
+        }}
+      >
+        {images.map((image, index) => (
+          <SwiperSlide key={image.id}>
+            <Img fluid={image.localFile.childImageSharp.fluid} key={image.id} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </>
   )
 }
 
 const DetailPageCarousel = props => {
   console.log('React Slider images', props.images)
+
+  const [currentSlide, setCurrentSlide] = useState(0)
+
   return (
     <>
       <CarouselContainer>
